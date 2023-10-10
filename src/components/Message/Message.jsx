6eb0645047589e9ccceef31b12b56be9cf09431e
MohaMed AshRaf <mohmmedashrraf@gmail.com>
2023-10-11 01:57:38 +0300

    Messages

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Message.module.css'
+
+export default function Message() {
+  return (
+    <div>Message</div>
+  )
+}
+//npm i generate-react-cli
+//npx generate-react-cli component Home يكريت الكومبوننت بنفس شكل التيمبلت
---
+++ src/components/MessageCard/MessageCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './MessageCard.module.css'
+
+export default function MessageCard() {
+  return (
+    <div>MessageCard</div>
+  )
+}
+//npm i generate-react-cli
+//npx generate-react-cli component Home يكريت الكومبوننت بنفس شكل التيمبلت
